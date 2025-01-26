10122c3ba9bf02a6fcb4528790c745b763e02eb7
John Tesla DEV <168617543+JohnyMaxida@users.noreply.github.com>
2025-01-27 00:12:16 +0700

    Add files via upload

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,13 +1,3 @@
-// import "./style.css";
-// import Header from "./../components/header/Header"
-
-import project1 from "./../img/projects/01.jpg";
-import project2 from "./../img/projects/02.jpg";
-import project3 from "./../img/projects/03.jpg";
-import project4 from "./../img/projects/04.jpg";
-import project5 from "./../img/projects/05.jpg";
-import project6 from "./../img/projects/06.jpg";
-
 const Contacts = () => {
     return (
     <main className="section">
